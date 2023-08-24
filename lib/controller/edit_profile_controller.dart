adb4bba1545c326574951fc93af004ca8bd79b6d
Sagar R Anghan <anghansagar25@gmail.com>
2023-08-24 20:56:23 +0530

    Integrate: Store details api

--- lib/controller/edit_profile_controller.dart
+++ lib/controller/edit_profile_controller.dart
@@ -1,11 +1,18 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:intl_phone_field/countries.dart';
+import 'package:supplier/constant/string_constant.dart';
+import 'package:supplier/model/store_profile_model.dart';
+import 'package:supplier/service/rest_service.dart';
+import 'package:supplier/utils/utils.dart';
 
 class EditProfileController extends GetxController {
   final TextEditingController ownerNameController = TextEditingController();
   final TextEditingController storeNameController = TextEditingController();
   final TextEditingController mobileNumberController = TextEditingController();
+  StoreProfileModel? storeProfileModel;
   Country countryMap = const Country(
     name: "India",
     nameTranslations: {
@@ -41,5 +48,20 @@ class EditProfileController extends GetxController {
   void onCountryChange(Country value) {
     countryMap = value;
     mobileNumberController.clear();
+  }
+
+  Future<void> getStoreProfile() async {
+    try {
+      final response = await RestServices.instance.getRestCall(
+        endpoint: RestConstants.instance.storeProfile,
+        addOns: '/${AppStringConstants.storeLogInId}',
+      );
+      if (response != null && response.isNotEmpty) {
+        storeProfileModel = storeProfileModelFromJson(response);
+      }
+      update();
+    } on SocketException catch (e) {
+      logs('Catch exception in getStoreProfile --> ${e.message}');
+    }
   }
 }
